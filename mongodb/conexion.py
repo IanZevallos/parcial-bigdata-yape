3c1017ca733d2d2a323e4ed3cf5dc5ec8f225e4d
IanZevallos <113573654+IanZevallos@users.noreply.github.com>
2026-06-25 00:06:58 -0500

    Update conexion.py

--- mongodb/conexion.py
+++ mongodb/conexion.py
@@ -1,1 +1,15 @@
-#conexion con mongodb
+from pymongo import MongoClient
+
+# 🔵 1. URL de conexión (Atlas)
+MONGO_URI = "mongodb+srv://<usuario>:<password>@cluster0.xxxxx.mongodb.net/?retryWrites=true&w=majority"
+
+# 🔵 2. Conectar a MongoDB
+client = MongoClient(MONGO_URI)
+
+# 🔵 3. Crear/seleccionar base de datos
+db = client["bigdata_s3_sunat"]
+
+# 🔵 4. Crear colección
+collection = db["empresas"]
+
+print("✅ Conexión exitosa a MongoDB Atlas")
